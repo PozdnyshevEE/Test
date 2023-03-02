fcf31f59f4fdf39cdc7b33251aef691de920b2fe
Евгений Позднышев <pozdnyshevEE@yandex.ru>
2023-03-02 17:35:09 +0300

    test

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.querySelector('#newGames').addEventListener('click', (event) => {
-    document.getElementById("collapseContainer1").style.display = "none";
-    document.getElementById("collapseContainer3").style.display = "none";
-    document.getElementById("collapseContainer2").style.display = "block";
+    document.querySelector('#collapseContainer1').style.display = "none";
+    document.querySelector('#collapseContainer3').style.display = "none";
+    document.querySelector('#collapseContainer2').style.display = "inline-flex";
+
 })
 
 document.querySelector('#start').addEventListener('click', (event) => {
-    document.getElementById("collapseContainer2").style.display = "none";
-    document.getElementById("collapseContainer3").style.display = "block";
+    document.querySelector('#collapseContainer2').style.display = "none";
+    document.querySelector('#collapseContainer3').style.display = "inline";
 })
 
 document.querySelector('#btnRetry').addEventListener('click', (event) => {
-    document.getElementById("collapseContainer3").style.display = "none";
-    document.getElementById("collapseContainer2").style.display = "block";
+    document.querySelector('#collapseContainer3').style.display = "none";
+    document.querySelector('#collapseContainer2').style.display = "inline-flex";
 })
-
