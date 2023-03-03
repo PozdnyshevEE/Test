0d27a25fe9c20b4f72227a5224ea4840cc7ed12e
Евгений Позднышев <pozdnyshevEE@yandex.ru>
2023-03-03 16:29:56 +0300

    test

--- script.js
+++ script.js
@@ -1,16 +1,88 @@
+
+
+
 document.querySelector('#newGames').addEventListener('click', (event) => {
     document.querySelector('#collapseContainer1').style.display = "none";
-    document.querySelector('#collapseContainer3').style.display = "none";
-    document.querySelector('#collapseContainer2').style.display = "inline-flex";
+    document.querySelector('#collapseContainer2').style.display = "block";
 
 })
 
 document.querySelector('#start').addEventListener('click', (event) => {
+    let minValue = document.querySelector('#min').value;
+    let maxValue = document.querySelector('#max').value;
+    minValue = (minValue < -999) ? minValue = -999 : (minValue > 999) ? minValue = 999 : minValue;
+    maxValue = (maxValue > 999) ? maxValue = 999 : (maxValue < -999) ? maxValue = -999 : maxValue;
+
+    if (Number.isNaN(maxValue) || Number.isNaN(minValue)) {
+        minValue = 0;
+        maxValue = 100;
+    }
+
+    let message = document.querySelector('#message');
+    message.innerText = `Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`;
+
+    let answerNumber  = Math.floor((minValue + maxValue) / 2);
+    let orderNumber = 1;
+    let gameRun = true;
+
+
+    let units = ['', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'];
+    let teens = ['', 'десять', 'одинадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
+    let dozens = ['', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'];
+    let hundreds = ['', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
+
+    function numberToText() { // Функция преобразования числа из цифр в слова (числа от -999 до 999).
+        let number = Math.abs(answerNumber);
+        let text = '';
+
+        if (number == 0) {
+            text = 'ноль';
+            return text;
+        }
+
+        if (number <= 9) {
+            return units[Math.floor(Math.abs(number) / 1)];
+        }
+
+        if (number > 9 && number < 20) {
+            return teens[Math.floor(number / 10 + number % 10)];
+        }
+
+        if (number >= 20 && number <= 99) {
+            return dozens[(Math.floor(number / 10)) - 1] + " " + units[Math.floor(number % 10)];
+        }
+
+        if (number >= 100 && number <= 999) {
+            return hundreds[Math.floor(number / 100)] + " " + numberToTextHundreds();
+        }
+    }
+
+    function numberToTextHundreds() { // Функция вычисления остатка от сотого числа и преобразования его в числа из цифр в слова (числа от 0 до 99) для последующего присоединения к функции numberToText() расчитывающей сотни hundreds.
+        let unitsTeensDozens = Math.abs(answerNumber) % 100;
+
+        if (unitsTeensDozens <= 9) {
+            return units[Math.floor(unitsTeensDozens / 1)];
+        }
+
+        if (unitsTeensDozens > 9 && unitsTeensDozens < 20) {
+            return teens[(Math.floor(unitsTeensDozens / 10)) + (unitsTeensDozens % 10)];
+        }
+
+        if (unitsTeensDozens >= 20 && unitsTeensDozens <= 99) {
+            return dozens[(Math.floor(unitsTeensDozens / 10)) - 1] + " " + units[Math.floor(unitsTeensDozens % 10)];
+        }
+    }
+
     document.querySelector('#collapseContainer2').style.display = "none";
-    document.querySelector('#collapseContainer3').style.display = "inline";
+    document.querySelector('#collapseContainer4').style.display = "block";
+})
+
+document.querySelector('#go').addEventListener('click', (event) => {
+    document.querySelector('#collapseContainer4').style.display = "none";
+    document.querySelector('#collapseContainer3').style.display = "block";
 })
 
 document.querySelector('#btnRetry').addEventListener('click', (event) => {
     document.querySelector('#collapseContainer3').style.display = "none";
-    document.querySelector('#collapseContainer2').style.display = "inline-flex";
+    document.querySelector('#collapseContainer2').style.display = "block";
 })
